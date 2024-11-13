23baef1ba0946108b33d18745891582b43e814e9
Leirbag <seravat.leirbag@gmail.com>
2024-11-13 17:19:29 -0300

    updating main func to log errors in the applcation

--- main.go
+++ main.go
@@ -2,13 +2,23 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"mctest-agent/internal/http"
 )
 
 func main() {
-	err := http.Init()
+    // Log the start of the application
+    log.SetFlags(0) // Remove detalhes de timestamp, etc.
+    log.Println("Starting mctest-agent...")
+
+    err := http.Init()
 
 	if err != nil {
 		fmt.Println(err)
-	}
+		log.Fatalf("Error initializing HTTP server: %v", err)
+    } else {
+        log.Println("HTTP server started successfully")
+    }
+    // Adiciona a linha separadora no final do arquivo
+    log.Println("\n=================\n")
 }
